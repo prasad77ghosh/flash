ce2dcc12b0414752bbf1d92048d98b4473dcfdd4
Prasad Ghosh <your_email@example.com>
2025-12-07 12:36:03 +0530

    small changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,6 +11,8 @@ const page = () => {
           <SingleChatCard/>
         </div>
       </div>
+
+      {/* right bar */}
       <div className="w-[75%] flex flex-col bg-background">
         <RightHeader />
         <div className="flex-1 overflow-auto">
